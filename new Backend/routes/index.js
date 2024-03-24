ba429475d1011450aad0c793f5f07d5ed0bdcbeb
chefpkj-rnf <piyush.jha@resourcifi.com>
2024-03-24 14:45:45 +0530

    adding new files

--- new Backend/routes/index.js
+++ new Backend/routes/index.js
@@ -1,8 +1,8 @@
-import express from "express";
+import {Router} from "express";
 import authRoutes from "./authRoutes.js";
 import notesRoutes from "./notesRoutes.js";
 
-const router = express.Router();
+const router = Router();
 
 
 router.use(authRoutes);
